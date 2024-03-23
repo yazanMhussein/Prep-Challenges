de14754c66d4447302298af8e7f1cb30ae2e8542
yazanmike <yazan.mike@gmail.com>
2024-03-23 13:40:31 +0300

    new file:   code-challenges/challenges-06.js 	new file:   package-lock.json 	new file:   test/challenges-06.test.js

---
+++ code-challenges/challenges-06.js
@@ -0,0 +1,56 @@
+'use strict';
+
+// -------------------------------------------------------------------------------------------------------
+//  Challenge 01:
+//  Required:
+//
+//  Write a function that takes an array and returns the maximum value in the provided array
+//  without using build-in functions.
+//  
+// Input: [12, 32, 22, 45, 78, 12, 50]
+// Output: 78
+//  
+
+const findMax = (arr)=>{
+    let max;
+    console.log(arr);
+    console.log(max);
+    return max;
+}
+console.log(max+ "oustide the funic");
+// -------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------
+//  Challenge 02:
+//  Required:
+//
+//  Write a function that takes an arrays and returns the Sum of the numeric values
+//  inside the array (there could be strings inside), without using built-in functions
+//  
+// Input: [20, '234', 'car', 41, 20, 'chair']
+// Output: 81
+//  
+
+const sumNums = (arr)=>{
+    let sum;
+    // write your code here
+    return sum;
+}
+// -------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------
+//  Challenge 03:
+//  Required:
+//
+//  Write a function that takes an array and returns the array reversed
+//  without using built-in functions
+//  
+// Input: ['C#', 'JS', 'Ruby','Python'] 
+// Output: ['Python','Ruby','JS','C#']
+
+const reverseArray = (arr)=>{
+    // write your code here
+}
+// -------------------------------------------------------------------------------------------------------
+
+module.exports = {findMax , sumNums, reverseArray};
